cb6a580a6c0057f799b1e8d7a2669ccccba480e8
cmortencio23-0328 <cmortencio23-0328@cca.edu.ph>
2026-06-14 03:13:18 +0800

    Add Firebase env variable fallback for Render

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,13 @@ const { cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const path = require('path');
 
-const serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = require(path.join(__dirname, 'serviceAccountKey.json'));
+}
 
 const app = initializeApp({
     credential: cert(serviceAccount)
